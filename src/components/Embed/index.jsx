773ff3f06e073c5f68e49c9f9578fc17c05f1d80
Downzin01 <downmestre@gmail.com>
2024-07-12 12:39:44 -0300

    Componentes da Embed do HomePage (fix + skeleton loader)

--- src/components/Embed/index.jsx
+++ src/components/Embed/index.jsx
@@ -20,7 +20,7 @@ export default function Embed() {
     return (
         <main>
             <section className={styles.card}>
-                <div className={`${styles.card__container} ${isLoading ? '' : styles.fadeIn}`}>
+                <div className={styles.card__container}>
                     <div className={styles.card__profile}>
                         {isLoading ? (
                             <>
@@ -35,7 +35,7 @@ export default function Embed() {
                         ) : (
                             <img
                                 id="profile_image"
-                                className={styles.card__image}
+                                className={`${styles.card__image} ${isLoading ? '' : styles.fadeIn}`}
                                 src="https://github.com/downzin01.png"
                                 srcSet="https://github.com/downzin01.png 1x, https://github.com/downzin01@2x.png 2x"
                                 alt="Imagem de perfil de Downzin"
